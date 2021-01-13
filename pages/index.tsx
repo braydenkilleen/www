4146c83ebdb112c0e91e033e50f799ad98cf46bf
Brayden Killeen <me@braydenkilleen.com>
2021-01-13 23:32:55 +1100

    Added styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello, I'm Brayden Killeen</h1>
-    <p>
-      I'm a computer science graduate and software engineer based in Melbourne,
-      Australia.
-    </p>
+    <header>
+      <h1>Hello, I'm Brayden Killeen</h1>
+      <p>
+        I'm a computer science graduate and software engineer based in
+        Melbourne, Australia.
+      </p>
+    </header>
   </Layout>
 );
 
